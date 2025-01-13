0408a35fa0168a44ef062d1de65475d5ea7a781e
shaunLaren <codelaren@gmail.com>
2025-01-13 02:56:54 +0100

    word counter

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    print(text)
+    count = word_count(text)
+    print(f"word count: {count}")
 
 def get_book_text(book_path):
     with open(book_path) as f:
          return f.read()
     
+def word_count(text):
+    return len(text.split())
+
+
 main()
 
 
